16e28648bdd22259aa3261076a52060833bc3c91
testcovidprueba <95432483+testcovidprueba@users.noreply.github.com>
2021-12-02 10:17:05 -0600

    fisrt commit

---
+++ covid.js
@@ -0,0 +1,69 @@
+
+
+
+$( window ).load(function() {
+  let searchParams = new URLSearchParams(window.location.search);
+  let clave = searchParams.get('id1');
+  let codigo = searchParams.get('id2');
+  $('#id1').val(clave);
+  $('#id2').val(codigo);
+});
+
+
+
+
+$('body').click(function() {
+   $('#modalcontenedor').hide();
+});
+
+$('#buscar').click(function(event){
+	
+  var noclickeo = $('#yaclickeo').val();
+  var id1 =  $('#id1').val();
+  var id2 =  $('#id2').val();
+  
+  if(noclickeo == 0){
+	$('#modalerror').show();
+
+  } else {
+
+  		if (  (id1=="123") && (id2="321") ) {
+
+		$('#modalcontenedor').show();
+             document.getElementById('modalcontenedor').style.opacity = "1";
+             document.getElementById('modal').style.opacity = "1";
+ 			 event.stopPropagation();
+
+  		} else { $('#modalerror').show();  }
+
+      }
+
+});
+
+$("#modalcontenedor").click(function(e){
+    e.stopPropagation();
+});
+
+$("#botoncerrar").click(function(e){
+    $('#modalcontenedor').hide();
+});
+
+$("#okdeerror").click(function(e){
+    $('#modalerror').hide();
+});
+
+
+$("#contendorrecaptcha").click(function(e){
+	var noclickeo = $('#yaclickeo').val();
+
+	if(noclickeo == 0 ) {
+	$('#cuadraditocaptcha').hide();
+	$('#spinner').show(1).delay(1100).hide(1);
+	$('#checkimagen').delay(1000).show(1)
+	$('#yaclickeo').val(1);
+	}
+
+
+});
+
+
